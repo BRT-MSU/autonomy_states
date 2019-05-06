a2449a8f99fb7fc87e59aaa1c3576c4b7f51b700
Kyle Melton <fs2dwyab@gmail.com>
2019-05-06 14:59:30 -0400

    Simplifying state logic

--- state_machine.py
+++ state_machine.py
@@ -1,12 +1,12 @@
 import robot_states as states
 
 class StateMachine(object):
-    states_list = {'startup', 'navigation', 'pathing', 'mining', 'dumping'}
 
     def __init__(self, controller):
         self.controller = controller
-        self.current_state = states.StartUpState()
-        self.previous_state = []
+        self.current_state = None
+        self.previous_states = []
+        self.threads = [] #used to keep track of all threads
 
     def on_event(self, event):
         return self
@@ -15,66 +15,33 @@ class StateMachine(object):
         self.previous_state = self.current_state
         self.current_state = new_state
 
+    def run_state(state):
+        self.threads.append(state)
+        state.start()
+        state.join()
+        self.threads.remove(state)
+
     def run(self):
-        states_list = ['nav', 'mine', 'dump']
-        threads = [] #used to keep track of all threads
         startup_state = states.StartUpState()
         startup_state.start()
         threads.append(startup_state)
         startup_state.join() # wait for startup to complete
         threads.remove(startup_state)
-        ball_tracking_state = states.BallTrackingState()
+        
+        ball_tracking_state = states.BallTrackingState() # run tracking constantly
         ball_tracking_state.start()
         threads.append(ball_tracking_state)
 
         while self.controller.isAutonomyActivated:
-            # navigation_state = states.NavigationState()
-            # navigation_state.start()
-            # threads.append(navigation_state)
-            # navigation_state.join()
-            # threads.remove(navigation_state)
-            #
-            # mining_state = states.MiningState()
-            # mining_state.start()
-            # threads.append(mining_state)
-            # mining_state.join()
-            # threads.remove(mining_state)
-            #
-            # navigation_state = states.NavigationState()
-            # navigation_state.start()
-            # threads.append(navigation_state)
-            # navigation_state.join()
-            # threads.remove(navigation_state)
-            #
-            # dumping_state = states.DumpingState()
-            # dumping_state.start()
-            # threads.append(dumping_state)
-            # threads.remove(dumping_state)
+            self.run_state(states.NavigationState(self.controller))
+            self.run_state(states.MiningState(self.controller))
+            self.run_state(states.NavigationState(self.controller)) 
+            self.run_state(states.DumpingState(self.controller))
+         
 
-            for i, state in enumerate(states_list):
-                if states_list[i] == 'nav':
-                    state = states.NavigationState()
-                elif states_list[i] == 'mine':
-                    state = states.MiningState()
-                elif states_list[i] == 'dump':
-                    state = states.DumpingState()
-
-                state.start()
-                threads.append(state)
-                if states_list[i] == 'dump':
-                    pass
-                else:
-                    state.join()
-                threads.remove(state)
-
-
-
-        for thread in threads: #safely end all threads
+        for thread in self.threads: #safely end all threads
             thread.join()
 
-
-
 if __name__ == '__main__':
-    robot = StateMachine()
-    robot.on_event('startup')
+    robot = StateMachine(None)
 
